79bbd0167e852bd7ee799a31ee750211cd145e17
Julie P <christynph2@outlook.com>
2020-07-09 20:07:24 -0500

    Add recipe exercise

---
+++ script.js
@@ -0,0 +1,14 @@
+var two = 2
+var onefourth = "1/4"
+
+function marinaraSauce (cookName) {
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!")
+    console.log("- Heat a medium-large saucepan over medium heat.")
+    console.log("- Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about " + two + " minutes.")
+    console.log("- Add " + onefourth + " cup water, " + two + " cans of crushed tomatoes, and salt and season with black pepper to taste.")
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes.")
+    console.log("- Stir in " + onefourth + " cup roughly chopped fresh basil, salt and pepper as needed.")
+    
+}
+
+marinaraSauce("Adam");
